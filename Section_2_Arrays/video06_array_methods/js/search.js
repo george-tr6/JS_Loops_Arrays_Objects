434b12b12de07452feb8c72ef49ee1f74e1e2925
george-tr6 <george.tr6@gmail.com>
2018-10-02 15:21:55 -0700

    video6 completed

--- Section_2_Arrays/video06_array_methods/js/search.js
+++ Section_2_Arrays/video06_array_methods/js/search.js
@@ -5,3 +5,19 @@ function print(message) {
   document.write( '<p>' + message + '</p>');
 }
 
+while (true) {
+  search = prompt("Search for a product. Type 'search' to show all lists and 'quit' to exit ")
+  search = search.toLowerCase();  
+  if (search === 'quit'){
+      break;
+    }else if (search === 'list'){
+      print (inStock.join(','));
+    }else {
+      if( inStock.indexOf(search) > -1 ) {
+        print('Yes, we have ' + search + ' in the store.');
+      }else {
+        print(search + ' is not is stock.');
+      }
+    }
+
+}
